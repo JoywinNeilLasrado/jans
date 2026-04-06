0e92c96991915a08f3c46b82f8df06386d2d6746
pujavs <pujas.works@gmail.com>
2026-04-06 21:58:51 +0530

    feat(config-api): shibboleth endpoint - wip
    Signed-off-by: pujavs <pujas.works@gmail.com>

--- jans-config-api/plugins/shibboleth-plugin/src/main/java/io/jans/configapi/plugin/shibboleth/rest/ShibbolethResource.java
+++ jans-config-api/plugins/shibboleth-plugin/src/main/java/io/jans/configapi/plugin/shibboleth/rest/ShibbolethResource.java
@@ -4,14 +4,20 @@ import io.jans.configapi.core.rest.BaseResource;
 import io.jans.configapi.core.rest.ProtectedApi;
 import io.jans.configapi.plugin.shibboleth.form.TrustRelationshipForm;
 import io.jans.configapi.plugin.shibboleth.model.ShibbolethIdpConfiguration;
+import io.jans.configapi.plugin.shibboleth.model.TrustRelationship;
 import io.jans.configapi.plugin.shibboleth.model.TrustedServiceProvider;
 import io.jans.configapi.plugin.shibboleth.service.ShibbolethService;
 import io.jans.configapi.plugin.shibboleth.util.Constants;
+import io.jans.configapi.util.ApiConstants;
+import io.jans.model.SearchRequest;
+import io.jans.orm.model.PagedResult;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
+
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -25,6 +31,10 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+
+import static io.jans.as.model.util.Util.escapeLog;
+
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.*;
@@ -40,6 +50,19 @@ import org.slf4j.Logger;
 @Consumes(MediaType.APPLICATION_JSON)
 @ApplicationScoped
 public class ShibbolethResource extends BaseResource {
+    
+    private static final String SAML_TRUST_RELATIONSHIP = "Trust Relationship";
+    private static final String SAML_TRUST_RELATIONSHIP_FORM = "Trust Relationship From";
+    private static final String SAML_TRUST_RELATIONSHIP_CHECK_STR = "Trust Relationship identified by '";
+    private static final String NAME_CONFLICT = "NAME_CONFLICT";
+    private static final String NAME_CONFLICT_MSG = "Trust Relationship with same name `%s` already exists!";
+    private static final String DATA_NULL_CHK = "RESOURCE_IS_NULL";
+    private static final String DATA_NULL_MSG = "`%s` should not be null!";
+    
+
+    private class TrustRelationshipPagedResult extends PagedResult<TrustRelationship> {
+    };
+
 
     @Inject
     private Logger logger;
@@ -89,17 +112,33 @@ public class ShibbolethResource extends BaseResource {
                     @SecurityRequirement(name = "oauth2", scopes = { Constants.SHIBBOLETH_TR_WRITE_ACCESS }),
                     @SecurityRequirement(name = "oauth2", scopes = { Constants.SHIBBOLETH_TR_ADMIN_ACCESS }) })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Trusted service providers", content = @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = TrustedServiceProvider.class)))),
+            @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = TrustRelationshipPagedResult.class), examples = @ExampleObject(name = "Response json example", value = "example/shibboleth/trust-relationship/get-shibboleth-trust"))),
             @ApiResponse(responseCode = "401", description = "Unauthorized"),
             @ApiResponse(responseCode = "500", description = "Internal Server Error") })
     @GET
     @Path("/trust")
     @ProtectedApi(scopes = { Constants.SHIBBOLETH_TR_READ_ACCESS }, groupScopes = {
             Constants.SHIBBOLETH_TR_WRITE_ACCESS }, superScopes = { Constants.SHIBBOLETH_TR_ADMIN_ACCESS })
-    public Response getTrustedServiceProviders() {
+    public Response getTrustedServiceProviders(
+            @Parameter(description = "Search size - max size of the results to return") @DefaultValue(ApiConstants.DEFAULT_LIST_SIZE) @QueryParam(value = ApiConstants.LIMIT) int limit,
+            @Parameter(description = "Search pattern") @DefaultValue("") @QueryParam(value = ApiConstants.PATTERN) String pattern,
+            @Parameter(description = "The 1-based index of the first query result") @DefaultValue(ApiConstants.DEFAULT_LIST_START_INDEX) @QueryParam(value = ApiConstants.START_INDEX) int startIndex,
+            @Parameter(description = "Attribute whose value will be used to order the returned response") @DefaultValue(ApiConstants.INUM) @QueryParam(value = ApiConstants.SORT_BY) String sortBy,
+            @Parameter(description = "Order in which the sortBy param is applied. Allowed values are \"ascending\" and \"descending\"") @DefaultValue(ApiConstants.ASCENDING) @QueryParam(value = ApiConstants.SORT_ORDER) String sortOrder,
+            @Parameter(description = "Field and value pair for searching", examples = @ExampleObject(name = "Field value example", value = "applicationType=web,persistClientAuthorizations=true")) @DefaultValue("") @QueryParam(value = ApiConstants.FIELD_VALUE_PAIR) String fieldValuePair)
+     {
         logger.debug("GET /shibboleth/trust");
-        List<TrustedServiceProvider> providers = shibbolethService.getTrustedServiceProviders();
-        return Response.ok(providers).build();
+        if (logger.isDebugEnabled()) {
+            logger.debug(
+                    "Shibboleth trust search param - limit:{}, pattern:{}, startIndex:{}, sortBy:{}, sortOrder:{}, fieldValuePair:{}",
+                    escapeLog(limit), escapeLog(pattern), escapeLog(startIndex), escapeLog(sortBy),
+                    escapeLog(sortOrder), escapeLog(fieldValuePair));
+        }
+        SearchRequest searchReq = createSearchRequest(shibbolethService.getDnForTrustRelationship(null), pattern, sortBy, sortOrder,
+                startIndex, limit, null, null, shibbolethService.getRecordMaxCount(),
+                fieldValuePair, TrustRelationship.class);
+
+        return Response.ok(this.doSearch(searchReq)).build();
     }
 
     @Operation(summary = "Gets a trusted service provider", description = "Gets a trusted SAML service provider by entity ID", operationId = "get-shibboleth-trust-by-id", tags = {
@@ -132,20 +171,25 @@ public class ShibbolethResource extends BaseResource {
             "Shibboleth IDP - Config Management" }, security = {                 
                     @SecurityRequirement(name = "oauth2", scopes = { Constants.SHIBBOLETH_TR_WRITE_ACCESS }),
                     @SecurityRequirement(name = "oauth2", scopes = { Constants.SHIBBOLETH_TR_ADMIN_ACCESS }) })
+    @RequestBody(description = "Trust Relationship object", content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA, schema = @Schema(implementation = TrustRelationshipForm.class), examples = @ExampleObject(name = "Request example", value = "example/shibboleth/trust-relationship/trust-relationship-post.json")))
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Created trusted service provider", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = TrustedServiceProvider.class))),
+            @ApiResponse(responseCode = "201", description = "Newly created Trust Relationship", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = TrustRelationship.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request" , content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ApiError.class, description = "BadRequestException"))),
             @ApiResponse(responseCode = "401", description = "Unauthorized"),
-            @ApiResponse(responseCode = "409", description = "Conflict - Entity already exists"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error") })
+            @ApiResponse(responseCode = "404", description = "Not Found" , content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ApiError.class, description = "NotFoundException"))),
+            @ApiResponse(responseCode = "500", description = "InternalServerError", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ApiError.class, description = "InternalServerError"))),
+            })
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
     @POST
     @Path("/trust")
     @ProtectedApi(scopes = { Constants.SHIBBOLETH_TR_WRITE_ACCESS }, groupScopes = { }, superScopes = { Constants.SHIBBOLETH_TR_ADMIN_ACCESS })
-    public Response addTrustedServiceProvider(@Valid @NotNull TrustedServiceProvider serviceProvider) {
+    public Response addTrustedServiceProvider(@MultipartForm TrustRelationshipForm trustRelationshipForm,
+            InputStream metadatafile) {
         logger.info("POST /shibboleth/trust");
 
-        if (serviceProvider.getEntityId() == null) {
-            return Response.status(Response.Status.BAD_REQUEST).entity("Entity ID is required").build();
-        }
+       // if (serviceProvider.getEntityId() == null) {
+        //    return Response.status(Response.Status.BAD_REQUEST).entity("Entity ID is required").build();
+       // }
 
         TrustedServiceProvider existing = shibbolethService.getTrustedServiceProvider(serviceProvider.getEntityId());
         if (existing != null) {
@@ -227,6 +271,33 @@ public class ShibbolethResource extends BaseResource {
     public Response getIdpMetadata() {
         logger.debug("GET /shibboleth/metadata");
         return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+    }
+    
+    private TrustRelationshipPagedResult doSearch(SearchRequest searchReq) {
+           
+        if (logger.isInfoEnabled()) {
+            logger.info("TrustRelationship search params - searchReq:{}", escapeLog(searchReq));
+        }
+
+        PagedResult<TrustRelationship> pagedResult = shibbolethService.getTrustRelationship(searchReq);
+        if (logger.isTraceEnabled()) {
+            logger.debug("PagedResult  - pagedResult:{}", pagedResult);
+        }
+
+        TrustRelationshipPagedResult pagedTrustRelationship = new TrustRelationshipPagedResult();
+        if (pagedResult != null) {
+            logger.debug("TrustRelationship fetched  - pagedResult.getEntries():{}", pagedResult.getEntries());
+            List<TrustRelationship> trustRelationships = pagedResult.getEntries();
+
+            pagedTrustRelationship.setStart(pagedResult.getStart());
+            pagedTrustRelationship.setEntriesCount(pagedResult.getEntriesCount());
+            pagedTrustRelationship.setTotalEntriesCount(pagedResult.getTotalEntriesCount());
+            pagedTrustRelationship.setEntries(trustRelationships);
+        }
+
+        logger.info("TrustRelationship pagedTrustRelationship:{}", pagedTrustRelationship);
+        return pagedTrustRelationship;
+
     }
 
 }
--- jans-config-api/plugins/shibboleth-plugin/src/main/java/io/jans/configapi/plugin/shibboleth/service/ShibbolethService.java
+++ jans-config-api/plugins/shibboleth-plugin/src/main/java/io/jans/configapi/plugin/shibboleth/service/ShibbolethService.java
@@ -1,34 +1,64 @@
 package io.jans.configapi.plugin.shibboleth.service;
 
+
+import io.jans.as.common.service.OrganizationService;
+import io.jans.as.common.util.AttributeConstants;
+import io.jans.configapi.configuration.ConfigurationFactory;
 import io.jans.configapi.plugin.shibboleth.model.ShibbolethIdpConfiguration;
 import io.jans.configapi.plugin.shibboleth.model.ShibbolethIdpConfigurationProperties;
+import io.jans.configapi.plugin.shibboleth.model.TrustRelationship;
 import io.jans.configapi.plugin.shibboleth.model.TrustedServiceProvider;
-import io.jans.orm.PersistenceEntryManager;
 
+import io.jans.configapi.util.ApiConstants;
+import io.jans.model.SearchRequest;
+import io.jans.orm.PersistenceEntryManager;
+import io.jans.orm.model.PagedResult;
+import io.jans.orm.model.SortOrder;
+import io.jans.orm.search.filter.Filter;
+import io.jans.util.StringHelper;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import org.slf4j.Logger;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @ApplicationScoped
 public class ShibbolethService {
 
-    private static final String SHIBBOLETH_CONFIG_DN = "ou=shibboleth-idp,ou=configuration,o=jans";
+    private static final String SHIBBOLETH_IDP_CONFIG_DN = "ou=shibboleth-idp,ou=configuration,o=jans";
+    private static final String SHIBBOLETH_TR_CONFIG_DN = "inum=%s,ou=trustRelationships,%s";
 
     @Inject
     private Logger logger;
+    
+    @Inject
+    ConfigurationFactory configurationFactory;
+    
+    @Inject
+    OrganizationService organizationService;
 
     @Inject
     private PersistenceEntryManager persistenceEntryManager;
+    
+
 
     public ShibbolethIdpConfiguration getConfiguration() {
         logger.debug("Fetching Shibboleth IDP configuration");
         
         try {
-            return persistenceEntryManager.find(ShibbolethIdpConfiguration.class, SHIBBOLETH_CONFIG_DN);
+            return persistenceEntryManager.find(ShibbolethIdpConfiguration.class, SHIBBOLETH_IDP_CONFIG_DN);
         } catch (Exception e) {
             logger.warn("Shibboleth configuration not found, returning defaults");
             return createDefaultConfiguration();
@@ -40,13 +70,161 @@ public class ShibbolethService {
         
         configuration.setRevision(configuration.getRevision() + 1);
         
-        if (persistenceEntryManager.contains(SHIBBOLETH_CONFIG_DN, ShibbolethIdpConfiguration.class)) {
+        if (persistenceEntryManager.contains(SHIBBOLETH_IDP_CONFIG_DN, ShibbolethIdpConfiguration.class)) {
             persistenceEntryManager.merge(configuration);
         } else {
-            configuration.setDn(SHIBBOLETH_CONFIG_DN);
+            configuration.setDn(SHIBBOLETH_IDP_CONFIG_DN);
             persistenceEntryManager.persist(configuration);
         }
+    }    
+
+    public String generateInumForNewRelationship() {
+        String newInum = null;
+        String newDn = null;
+        do {
+            newInum = UUID.randomUUID().toString();
+            newDn = getDnForTrustRelationship(newInum);
+        } while (containsRelationship(newDn));
+
+        return newInum;
     }
+    
+    public String getDnForTrustRelationship(String inum) {
+        String orgDn = organizationService.getDnForOrganization();
+        if (StringHelper.isEmpty(inum)) {
+            return String.format("ou=trustRelationships,%s", orgDn);
+        }
+        return String.format(SHIBBOLETH_TR_CONFIG_DN, inum, orgDn);
+    }
+    
+    public boolean containsRelationship(String dn) {
+        return persistenceEntryManager.contains(dn, TrustRelationship.class);
+    }
+    
+    public List<TrustRelationship> searchTrustRelationship(String pattern, int sizeLimit) {
+
+        logger.debug("Search TrustRelationship with pattern:{}, sizeLimit:{}", pattern, sizeLimit);
+
+        String[] targetArray = new String[] { pattern };
+        Filter displayNameFilter = Filter.createSubstringFilter(AttributeConstants.DISPLAY_NAME, null, targetArray,
+                null);
+        Filter descriptionFilter = Filter.createSubstringFilter(AttributeConstants.DESCRIPTION, null, targetArray,
+                null);
+        Filter inumFilter = Filter.createSubstringFilter(AttributeConstants.INUM, null, targetArray, null);
+        Filter searchFilter = Filter.createORFilter(displayNameFilter, descriptionFilter, inumFilter);
+
+        logger.debug("Search TrustRelationship with searchFilter:{}", searchFilter);
+        return persistenceEntryManager.findEntries(getDnForTrustRelationship(null), TrustRelationship.class, searchFilter, sizeLimit);
+    }
+
+    public List<TrustRelationship> getAllTrustRelationship(int sizeLimit) {
+        return persistenceEntryManager.findEntries(getDnForTrustRelationship(null), TrustRelationship.class, null, sizeLimit);
+    }
+
+    public List<TrustRelationship> getAllTrustRelationship() {
+        return persistenceEntryManager.findEntries(getDnForTrustRelationship(null), TrustRelationship.class, null);
+    }
+
+    public PagedResult<TrustRelationship> getTrustRelationship(SearchRequest searchRequest) {
+        logger.debug("Search TrustRelationship with searchRequest:{}", searchRequest);
+
+        Filter searchFilter = null;
+        List<Filter> filters = new ArrayList<>();
+        if (searchRequest.getFilterAssertionValue() != null && !searchRequest.getFilterAssertionValue().isEmpty()) {
+
+            for (String assertionValue : searchRequest.getFilterAssertionValue()) {
+                String[] targetArray = new String[] { assertionValue };
+                Filter displayNameFilter = Filter.createSubstringFilter(AttributeConstants.DISPLAY_NAME, null,
+                        targetArray, null);
+                Filter descriptionFilter = Filter.createSubstringFilter(AttributeConstants.DESCRIPTION, null,
+                        targetArray, null);
+                Filter inumFilter = Filter.createSubstringFilter(AttributeConstants.INUM, null, targetArray, null);
+                filters.add(Filter.createORFilter(displayNameFilter, descriptionFilter, inumFilter));
+            }
+            searchFilter = Filter.createORFilter(filters);
+        }
+
+        logger.trace("TrustRelationship pattern searchFilter:{}", searchFilter);
+        List<Filter> fieldValueFilters = new ArrayList<>();
+        if (searchRequest.getFieldValueMap() != null && !searchRequest.getFieldValueMap().isEmpty()) {
+            for (Map.Entry<String, String> entry : searchRequest.getFieldValueMap().entrySet()) {
+                Filter dataFilter = Filter.createEqualityFilter(entry.getKey(), entry.getValue());
+                logger.trace("TrustRelationship dataFilter:{}", dataFilter);
+                fieldValueFilters.add(Filter.createANDFilter(dataFilter));
+            }
+            searchFilter = Filter.createANDFilter(Filter.createORFilter(filters),
+                    Filter.createANDFilter(fieldValueFilters));
+        }
+
+        logger.debug("TrustRelationship searchFilter:{}", searchFilter);
+
+        return persistenceEntryManager.findPagedEntries(getDnForTrustRelationship(null), TrustRelationship.class, searchFilter, null,
+                searchRequest.getSortBy(), SortOrder.getByValue(searchRequest.getSortOrder()),
+                searchRequest.getStartIndex(), searchRequest.getCount(), searchRequest.getMaxCount());
+
+    }
+    
+    public int getRecordMaxCount() {
+        logger.trace(" MaxCount details - ApiAppConfiguration.MaxCount():{}, DEFAULT_MAX_COUNT:{} ",
+                configurationFactory.getApiAppConfiguration().getMaxCount(), ApiConstants.DEFAULT_MAX_COUNT);
+        return (configurationFactory.getApiAppConfiguration().getMaxCount() > 0
+                ? configurationFactory.getApiAppConfiguration().getMaxCount()
+                : ApiConstants.DEFAULT_MAX_COUNT);
+    }
+
+    public TrustRelationship getTrustRelationshipByDn(String dn) {
+        try {
+            return persistenceEntryManager.find(TrustRelationship.class, dn);
+        } catch (Exception e) {
+            logger.warn("", e);
+            return null;
+        }
+    }
+    
+    public TrustRelationship addTrustRelationship(TrustRelationship trustRelationship, InputStream file)
+     {
+        logger.info("Add new trustRelationship:{}, file:{}", trustRelationship, file);
+
+        setTrustRelationshipDefaultValue(trustRelationship, false);
+        persistenceEntryManager.persist(trustRelationship);
+
+        if (file != null && file.available() > 0) {
+            saveSpMetaDataFileSourceTypeFile(trustRelationship, file);
+        }else {
+            trustRelationship.setSpMetaDataFN(null);
+        }
+        
+        persistenceEntryManager.merge(trustRelationship);
+        logger.info("After saving new trustRelationship:{}", trustRelationship);
+        return getTrustRelationshipByInum(trustRelationship.getInum());
+    }
+
+    private TrustRelationship setTrustRelationshipDefaultValue(TrustRelationship trustRelationship, boolean update) {
+        logger.debug("trustRelationship:{}, update:{}",trustRelationship, update);
+        return trustRelationship;
+    }
+    
+    private boolean saveSpMetaDataFileSourceTypeFile(TrustRelationship trustRelationship, InputStream file) {
+
+        log.debug("saveSpMetadataFileSourceTypeFile(). trustRelationship: {} . file: {}",trustRelationship,file);
+    
+        final String spMetadataFileName = getSpNewMetadataFileName(trustRelationship);
+        trustRelationship.setSpMetaDataFN(spMetadataFileName);
+        InputStream targetStream = file;
+        final String metadataFilePath  = samlIdpService.saveMetadataFile(
+                samlConfigService.getSpMetadataDir(),spMetadataFileName,Constants.SP_MODULE,targetStream);
+        log.debug("targetStream: {}, spMetadataDir: {}, spMetadataFileName: {}",targetStream,samlConfigService.getSpMetadataDir(),spMetadataFileName);
+        if(StringHelper.isNotEmpty(metadataFilePath)) {
+            trustRelationship.setSpMetaDataFN(metadataFilePath);
+            log.debug("SP Metadata file ' {} ' saved.",spMetadataFileName);
+            return true;
+        }else {
+            log.error("Failed to save SP metadata file for TrustRelationship ' {} '",trustRelationship.getInum());
+            return false;
+        }
+    }
+    
+    //----------------
 
     public List<TrustedServiceProvider> getTrustedServiceProviders() {
         logger.debug("Fetching trusted service providers");
@@ -109,7 +287,7 @@ public class ShibbolethService {
 
     private ShibbolethIdpConfiguration createDefaultConfiguration() {
         ShibbolethIdpConfiguration config = new ShibbolethIdpConfiguration();
-        config.setDn(SHIBBOLETH_CONFIG_DN);
+        config.setDn(SHIBBOLETH_IDP_CONFIG_DN);
         config.setRevision(1);
         
         ShibbolethIdpConfigurationProperties props = new ShibbolethIdpConfigurationProperties();
